a6681a522a270d2d1169678c805c52853eacfefb
CaioBattisti <cluizbattisti@gmail.com>
2025-02-18 20:49:35 -0300

    finalizado 4

--- atividade4_CaioLuizbattisti.py
+++ atividade4_CaioLuizbattisti.py
@@ -1,1 +1,9 @@
-#
+#Escreva um programa para pedir um número e em seguida o nome.
+#Exiba o nome (uma letra de cada vez em cada linha)
+#e repita isso para o número de vezes que ele digitou.
+nome =input("digite seu nome: ")
+num = int(input("digite um numero: "))
+for _ in range(num):
+    for letra in nome:
+        print(letra)
+        print("----")
