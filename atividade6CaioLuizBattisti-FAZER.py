ea4d5574ddacc933b528c763fc08b1a1f359ac8c
CaioBattisti <cluizbattisti@gmail.com>
2025-02-19 18:43:58 -0300

    finalizado 6

--- atividade6CaioLuizBattisti-FAZER.py
+++ atividade6CaioLuizBattisti-FAZER.py
@@ -2,3 +2,7 @@
 #certificando-se de mostrar o número que eles inseriram na saída.
 num1 =int(input("Digite um numero abaixo de 50: "))
 if num1 < 50:
+    for i in range(50, num1 -1,-1):
+        print(i)
+else:
+    print("numero invalido! Deve ser menor que 50!")
