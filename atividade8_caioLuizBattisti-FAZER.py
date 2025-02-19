5a96de7f226cb5671fd87d5ed9d0f5bb6adcd7cf
CaioBattisti <cluizbattisti@gmail.com>
2025-02-19 20:56:17 -0300

    fazendo 8

--- atividade8_caioLuizBattisti-FAZER.py
+++ atividade8_caioLuizBattisti-FAZER.py
@@ -2,11 +2,3 @@
 #após cada entrada,pergunte se ele deseja que esse número seja incluído (S ou s, N ou n).
 #Se ele desejar, adicione o número ao total. Se ele não quiser incluí-lo, não o adicione ao total.
 #Depois de inserir todos os cinco números, exiba o total.
-total = 0
-for _ in range(5):
-    num1 =int(input("Digite um numero: "))
-    incluir=input("Deseja inclur esse numero na soma?(S/N): ").strip().lower()
-if incluir == "S":
-    total += num1
-if incluir == "N":
-    print("o total é:{total}")
