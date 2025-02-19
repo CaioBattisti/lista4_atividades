7f83626804370ec85ddf91d0e6482d305a7239e3
CaioBattisti <cluizbattisti@gmail.com>
2025-02-18 21:44:10 -0300

    FAZER

---
+++ atividade8_caioLuizBattisti-FAZER.py
@@ -0,0 +1,6 @@
+#Defina uma variável chamada total como 0 (Zero). Peça ao usuário para inserir cinco números e
+#após cada entrada,pergunte se ele deseja que esse número seja incluído (S ou s, N ou n).
+#Se ele desejar, adicione o número ao total. Se ele não quiser incluí-lo, não o adicione ao total.
+#Depois de inserir todos os cinco números, exiba o total.
+total = (0)
+num1 =int(input("Digite um numero: "))
