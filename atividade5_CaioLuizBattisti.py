49a60d49d61b45ea9f391a7eaa4ee23e2efc071f
CaioBattisti <cluizbattisti@gmail.com>
2025-02-18 21:02:08 -0300

    finalizado 5

---
+++ atividade5_CaioLuizBattisti.py
@@ -0,0 +1,5 @@
+#Peça ao usuário para inserir um número que deseja a tabuada e, em seguida, exibir a tabuada para esse número.
+num1 =int(input("digite um numero: "))
+print(f"A tabuada do {num1}: ")
+for i in range(1, 11):
+    print(f"{num1} x {i} = {num1*i}")
