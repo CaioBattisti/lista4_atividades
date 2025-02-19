6a30ae1ea54fc792b858b5508282937bf07336b9
CaioBattisti <cluizbattisti@gmail.com>
2025-02-18 21:33:09 -0300

    6 fazer

---
+++ atividade6CaioLuizBattisti.py
@@ -0,0 +1,6 @@
+#Peça um número abaixo de 50 e, em seguida, faça uma contagem regressiva de 50 até esse número,
+#certificando-se de mostrar o número que eles inseriram na saída.
+num1 =float(input("Digite um numero abaixo de 50: "))
+for _ in range(num1):
+    for numeros in num1:
+        print(numeros)
