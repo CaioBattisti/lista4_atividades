49ab1db1e680917dc36b6e660170996d2538892b
CaioBattisti <cluizbattisti@gmail.com>
2025-02-19 21:49:21 -0300

    finalizado 8

--- atividade8_CaioLuizBattisti.py
+++ atividade8_CaioLuizBattisti.py
@@ -3,3 +3,9 @@
 #Se ele desejar, adicione o número ao total. Se ele não quiser incluí-lo, não o adicione ao total.
 #Depois de inserir todos os cinco números, exiba o total.
 total = 0
+for i in range(5):
+    num=int(input(f"Digite {i+1}° Numero: "))
+    resposta=input("Deseja incluir o numero? {num} (S/N): ")
+    if resposta.lower() =="s":
+        total += num
+    print("O Total é:",total)
